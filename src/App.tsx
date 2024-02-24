956ffc77bdd6da8d51be3b09e39fefe834902b05
obirikan <obirikan020@gmail.com>
2024-02-24 12:00:00 +0100

    dockerized

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React+New</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is {count} now
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
