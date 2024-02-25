792948995bf4495e65b60a768116fd564d5473dc
obirikan <obirikan020@gmail.com>
2024-02-25 18:50:54 +0100

    docker compose

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React+New</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count} now
+          count is {count} now working on new or old one coming home?
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
